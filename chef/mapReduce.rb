d68e0824bdff18fb4c0753960aa50b7e9e36bbd4
Mathis Neunzig <mathis.neunzig@gmail.com>
2023-12-12 10:21:51 +0000

    chef

---
+++ chef/mapReduce.rb
@@ -0,0 +1,31 @@
+# Ingredients
+Ingredients.
+32 g data
+32 g mapper
+32 g reducer
+32 g result
+
+# Method - Map
+Put data into the mixing bowl.
+Take data from the mixing bowl.
+Put mapper into the mixing bowl.
+Take mapper from the mixing bowl.
+Fold data into mapper.
+Take data from the mixing bowl.
+
+# Method - Shuffle
+Put mapper into the mixing bowl.
+Take mapper from the mixing bowl.
+Fold mapper into the mixing bowl.
+Take mapper from the mixing bowl.
+
+# Method - Reduce
+Put result into the mixing bowl.
+Take result from the mixing bowl.
+Put reducer into the mixing bowl.
+Take reducer from the mixing bowl.
+Fold result into reducer.
+Take result from the mixing bowl.
+
+# Display the result
+Serve result.
