293f7abaf31d0c89e43c8c6dcd26ce4411c14088
Mathis Neunzig <mathis.neunzig@gmail.com>
2024-03-16 16:45:12 +0100

    Add files via upload

---
+++ vorlesungen/20240318/java-springboot-ts-react-mongodb-jacoco/cats-backend/src/main/java/de/wi22sea/demo/cats/CatsApplication.java
@@ -0,0 +1,13 @@
+package de.wi22sea.demo.cats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsApplication.class, args);
+	}
+
+}
