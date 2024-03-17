10beac0aa2b8f65304c2eaf06b370043fa3552bc
Mathis Neunzig <mathis.neunzig@gmail.com>
2024-03-17 17:07:53 +0100

    :(

--- vorlesungen/20240318/js-nextjs-redis-redisjson-redissearch/pages/api/cars.js
+++
@@ -1,7 +0,0 @@
-import { createCar } from '../../lib/redis';
-
-export default async function handler(req, res) {
-    const id = await createCar(req.body);
-    console.log(id)
-    res.status(200).json({ id })
-}
